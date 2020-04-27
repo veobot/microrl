a4093f51430448b44eed6214f24df98e84efdf54
Daniel Willenson <daniel@veobot.com>
2020-04-27 15:13:59 -0400

    Veo microrl config

--- src/config.h
+++ src/config.h
@@ -27,13 +27,12 @@ Token is word separate by white space, for example 3 token line:
 Define you prompt string here. You can use colors escape code, for highlight you prompt,
 for example this prompt will green color (if you terminal supports color)*/
 //#define _PROMPT_DEFAULT "\033[32mIRin >\033[0m "	// green color
-#define _PROMPT_DEFAULT "\033[32mIRin >\033[0m "	// green color
-//#define _PROMPT_DEFAULT "IRin > "
+#define _PROMPT_DEFAULT "SP > "
 
 /*
 Define prompt text (without ESC sequence, only text) prompt length, it needs because if you use
 ESC sequence, it's not possible detect only text length*/
-#define _PROMPT_LEN       7
+#define _PROMPT_LEN       5
 
 /*Define it, if you wanna use completion functional, also set completion callback in you code,
 now if user press TAB calls 'copmlitetion' callback. If you no need it, you can just set 
@@ -65,11 +64,11 @@ If not defined, use my own u16int_to_str variant, it's save about 800 byte of co
 on AVR (avr-gcc build).
 Try to build with and without, and compare total code size for tune library.
 */
-#define _USE_LIBC_STDIO
+#undef _USE_LIBC_STDIO
 
 /*
 Enable 'interrupt signal' callback, if user press Ctrl+C */
-#define _USE_CTRL_C
+#undef _USE_CTRL_C
 
 /*
 Print prompt at 'microrl_init', if enable, prompt will print at startup, 
@@ -78,21 +77,11 @@ NOTE!: Enable it, if you call 'microrl_init' after your communication subsystem
 already initialize and ready to print message */
 #undef _ENABLE_INIT_PROMPT
 
-/*
-New line symbol */
-#define _ENDL_LF
+/* New line symbol for input*/
+#define _ENDL_CR
 
-#if defined(_ENDL_CR)
-#define ENDL "\r"
-#elif defined(_ENDL_CRLF)
+/* New line symbol for output */
 #define ENDL "\r\n"
-#elif defined(_ENDL_LF)
-#define ENDL "\n"
-#elif defined(_ENDL_LFCR)
-#define ENDL "\n\r"
-#else
-#error "You must define new line symbol."
-#endif
 
 /********** END CONFIG SECTION ************/
 
